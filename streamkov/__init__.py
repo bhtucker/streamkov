c53fc1c53fdcd9996480aeb54cfae4444cca796d
Benson Tucker <benson@chatid.com>
2016-05-11 01:29:54 -0400

    package-level tweepy and logger

--- streamkov/__init__.py
+++ streamkov/__init__.py
@@ -7,3 +7,30 @@
 """
 
 CONNECTION_STRING = 'postgresql://streamkov@localhost:5432/streamkov'
+
+import tweepy
+import logging
+from secrets import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_secret)
+
+
+def get_api():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    api = tweepy.API(auth)
+    return api
+
+api = get_api()
+
+LOG = 'streamkov.log'
+logFormatter = logging.Formatter('%(asctime)-15s %(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(20)
+file_handler = logging.FileHandler(LOG)
+file_handler.setLevel(20)
+file_handler.setFormatter(logFormatter)
+logger.addHandler(file_handler)
+logger.info('logging initialized')
